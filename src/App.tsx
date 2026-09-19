a31fa2a2a49da1439aaba13e575c451cf2c05078
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-20 05:55:33 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './component/shared/Navbar'
+import Footer from './component/shared/Footer'
+const App = () => {
+  return (
+
+    <div>
+
+    <Navbar />
+    <Footer />
+    </div>
+  );
+};
+
+export default App;
