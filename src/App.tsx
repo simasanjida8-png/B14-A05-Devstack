c28b1d26b02e7bbc5ef6d783fc54c0406238d42a
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-20 12:12:11 +0600

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './component/shared/Navbar'
 import Footer from './component/shared/Footer'
+
 const App = () => {
   return (
 
     <div>
 
     <Navbar />
+   
     <Footer />
     </div>
   );
--- src/component/shared/Navbar.tsx
+++ src/component/shared/Navbar.tsx
@@ -1,10 +1,43 @@
-
+import Logo from '../../assets/logo-text.png'
 
 const Navbar = () => {
     return (
-        <div>
+       <nav className="pt-6 bg-[#FFFFFF] ">
+        <div className="  container mx-auto flex justify-between items-center">
+            <div>
+                <a href="#">
+                    <img src={Logo} alt="Dev Stack" />
+                </a>
+            </div>
+           
+                <ul className='flex justify-center items-center gap-5'>
+                <li>
+                    <span className='text-[#DB2777]'>
+                        <a href="#">Home</a>
+                    </span>
+                </li>
+                <li>
+                    <a href="#">Technologies</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+               <li>
+                 <a href="#">About</a>
+               </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                </ul>
             
+            <div className=' flex gap-5'>
+                <button>Sign In</button>
+                <button className='border-transparent rounded-[25px] px-5 py-2.5  bg-[#DB2777] text-[#FFFFFF]'>Sign Up</button>
+                </div>
         </div>
+       </nav>
+         
+      
     );
 };
 
