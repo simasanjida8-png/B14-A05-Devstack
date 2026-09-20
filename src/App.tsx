cc69e3d13e98b765e89c926bc21f4fbceb53fe20
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-20 15:41:11 +0600

    create Technology file and create promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import Navbar from './component/shared/Navbar'
 import Footer from './component/shared/Footer'
 import Banner from './component/Banner';
+import Technology from './component/Technology';
+
+
+const technologyPromise = async() =>{
+  const res = await fetch('/data.json');
+  const data = await res.json();
+  return data
+
+}
+
+
 
 const App = () => {
+
+  console.log(technologyPromise)
   return (
 
     <div>
 
     <Navbar />
    <Banner />
+   <Technology technologyPromise={technologyPromise}/>
     <Footer />
     </div>
   );
---
+++ src/component/Technology.tsx
@@ -0,0 +1,13 @@
+
+
+const Technology = ({technologyPromise}) => {
+
+    console.log(technologyPromise)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Technology;
