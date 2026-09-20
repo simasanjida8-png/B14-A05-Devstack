0041d8e339ebf49701bf7809a0fade139570886a
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-20 18:12:42 +0600

    added button and card information

--- src/technology/Technologyys.tsx
+++ src/technology/Technologyys.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import type { TechnologyType } from "../type/Type"
 
 
@@ -6,13 +7,34 @@ export interface TechnologysProps {
 }
 
 export default function Technologys({ technology}: TechnologysProps) {
+  const [added, setAdded] = useState<boolean>(false)
+  const handleAddStack = () => {
+
+    setAdded(!added)
+  }
     
     return (
-      <div>
-        <h2>
+// top part
+
+      <div className={`rounded-2xl border-2 bg-white p-5 ${added ? "border-pink-800" : "border-gray-100"}`}>
+        <div>
+          <h2 className="text-xl font-bold">
           {technology.name}
         </h2>
-        <p>{technology.description}</p>
+        
+        <p className="mt-2 text-gray-500">{technology.description}</p>
+        </div>
+        <h2>{technology.level}</h2>
+        <h2>{technology.category}</h2>
+        <h2>{technology.badge}</h2>
+        <h2>{technology.rating}</h2>
+        
+        <button className="rounded" onClick={handleAddStack}>
+
+        {added? 'Added' : 'Add to Stack'}
+
+        </button>
+
       </div>
     )
 }
